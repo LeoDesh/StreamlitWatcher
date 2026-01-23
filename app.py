e77428e34a1a0809aa7e7ac33986d2344d859dc4
MG16 <dere_de@gmx.at>
2026-01-23 22:17:03 +0100

    renamed pages to views

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
-st.title("Start Page")
+# 1. Setup individual pages
+# You can rename the entry point (main.py) to "Home" or point to a separate file
+home_page = st.Page("views/home.py", title="Home",  default=True)
+dash_page = st.Page("views/distance.py", title="Analytics")
+settings_page = st.Page("views/units.py", title="App Settings")
+
+# 2. Organize into sidebar sections
+# By using a dictionary, you create collapsible headers in the sidebar
+navigation_menu = st.navigation(
+    [home_page,dash_page,settings_page]
+)
+
+
+# 4. EXECUTE the navigation (Crucial: The app will be blank without this)
+navigation_menu.run()
