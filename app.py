a4a8d596fade6902099f0971e2a02cebd8199464
MG16 <dere_de@gmx.at>
2026-02-13 17:43:51 +0100

    added positioning in navigation. icons with markdown logic.

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
+# MUST BE AT THE TOP - Target the specific navigation list container
 
-# 1. Setup individual pages
-# You can rename the entry point (main.py) to "Home" or point to a separate file
-home_page = st.Page("views/home.py", title="Home", icon="🏠", default=True)
-distance_page = st.Page("views/distance.py", title="Distance",icon="📈")
-units_page = st.Page("views/units.py", title="Units",icon="📋")
-speed_page = st.Page("views/speed.py", title="Speed",icon="📊")
-latest_activities_page = st.Page("views/latest_activities.py", title="Latest Activities",icon="🔢")
-# 2. Organize into sidebar sections
-# By using a dictionary, you create collapsible headers in the sidebar
+st.set_page_config(layout="wide")
+home_page = st.Page("views/home.py", title="Home",icon=":material/home:", default=True)
+distance_page = st.Page("views/distance.py", title="Distance",icon=":material/route:")
+units_page = st.Page("views/units.py", title="Units",icon=":material/table_view:")
+speed_page = st.Page("views/speed.py", title="Speed",icon=":material/speed:")
+latest_activities_page = st.Page("views/latest_activities.py", title="Latest Activities",icon=":material/search:")
+
 navigation_menu = st.navigation(
-    [home_page,units_page,speed_page,distance_page,latest_activities_page]
+    [home_page,units_page,speed_page,distance_page,latest_activities_page],position="top"
 )
 
 
-# 4. EXECUTE the navigation (Crucial: The app will be blank without this)
 navigation_menu.run()
